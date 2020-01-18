23bc24d9a6978195672269c10003e5c216ec6f7a
Kamaz <kamaz@xsimple.org>
2020-01-18 12:48:29 +0000

    feat: merge parameters and fix ci

---
+++ REAME.md
@@ -0,0 +1,59 @@
+# Status
+
+Still beta
+
+# Why
+
+Because switching between AWS credentials is annoying with mfa.
+
+# How
+
+1. Install [direnv](https://direnv.net/) or a clone but I've only tested in `direnv`.
+
+2. Create a `.envrc` file in your project with following configuration:
+
+```bash
+# .envrc
+dotenv
+```
+
+It is safe to check-in to repository.
+
+3. Add a file `.env` and configure with following properties
+
+```
+AWS_DEFAULT_REGION=eu-west-2
+AWS_PROFILE=default
+DAM_USER="replace_aws_user"
+DAM_ACCOUNT="replace_aws_account_number"
+```
+
+4. Use the token
+   - By typing the command
+
+```bash
+dotenv-aws-mfa -t <token>
+```
+
+    - By being asked
+
+```bash
+dotenv-aws-mfa
+```
+
+# Under the hood
+
+## Order of precedence
+
+1. Environment variables
+2. Arguments
+3. Interactive command line
+
+# Fancy contribute
+
+To install locally just run:
+
+```bash
+yarn build; yarn global add ${PWD}
+dotenv-aws-mfa
+```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 import program from "yargs";
-import { pickAll, merge, prop, map, filter, compose } from "ramda";
+import { existsSync } from "fs";
+import { join as joinPath } from "path";
+import { pickAll, merge, prop, map, filter, compose, mergeWith } from "ramda";
 import { updateAWSCredentials, Authentication } from "./update-aws-credentials";
 
+// console.log(process.cwd()); add as debug
 const userOption = {
   name: "user",
   message: "IAM user name"
@@ -42,6 +45,7 @@ const questions = [
 const pickOnlyOptions = pickAll(map(prop("name"))(questions));
 const argv: object = pickOnlyOptions(
   program
+    .usage("Usage:")
     .option("u", {
       alias: userOption.name,
       describe: userOption.message,
@@ -70,14 +74,28 @@ const envVars = {
   [profileOption.name]: process.env.AWS_PROFILE
 };
 
-const partialAnswers = merge(envVars)(argv);
-const isAnswered = (property: string) => partialAnswers[property] === undefined;
+// console.log("arguments", argv);
+// console.log("env variables", envVars);
+const definedValue = (a: unknown, b: unknown) => (a === undefined ? b : a);
+const partialAnswers = mergeWith(definedValue, envVars, argv);
+// console.log("partial answer", partialAnswers);
+const isAnswered = (property: string) => {
+  // console.log(partialAnswers, property);
+  return partialAnswers[property] === undefined;
+};
 const questionsToAsk = filter(
   // @ts-ignore
   compose(isAnswered, prop("name"))
 )(questions);
+// console.log("questions to ask", questionsToAsk);
 
 inquirer.prompt(questionsToAsk).then(async answers => {
   const finalAnswers = merge(partialAnswers)(answers) as Authentication;
-  updateAWSCredentials(finalAnswers);
+  const dotEnvFilePath = joinPath(process.cwd(), ".env");
+  if (existsSync(dotEnvFilePath)) {
+    updateAWSCredentials(finalAnswers);
+  } else {
+    console.error(`Dot env file is missing under ${process.cwd()}`);
+    process.exit(1);
+  }
 });
